a8226d84266ad649f5bc7b25da4c57fdb5d4e4ea
Shaan Prasad <prasadshaan4@gmail.com>
2024-07-21 23:26:07 +0530

    checkpoint

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+const JWT_SECRET = 'shaanSecret'
+module.export = JWT_SECRET
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 
 const mainRouter = require('./routes/index')
+const userRouter = require('./routes/user');
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.use('/api/v1/',mainRouter);
 
 
+
+app.listen(3000);
